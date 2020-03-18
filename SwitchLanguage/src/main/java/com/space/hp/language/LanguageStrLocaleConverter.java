00b0b9dd1ff936a7fcecd324284236d4872443f5
tianxiatianshan <1444672626@qq.com>
2020-03-18 16:59:36 +0800

    增加注释

--- SwitchLanguage/src/main/java/com/space/hp/language/LanguageStrLocaleConverter.java
+++ SwitchLanguage/src/main/java/com/space/hp/language/LanguageStrLocaleConverter.java
@@ -5,11 +5,19 @@ import java.util.Locale;
 /**
  * @author: HePing
  * @date: 2019/12/27
- * @description:
+ * @description: String和Locale对象转换器
  */
 public interface LanguageStrLocaleConverter {
 
+    /**
+    * String转换Locale；  
+    * setLanguage（）
+    */
     Locale string2Locale(String language);
 
+    /**
+    * Locale对象转换成String
+    * getCurrentLanguage（）
+    */
     String locale2String(Locale locale);
 }
